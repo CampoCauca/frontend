a870927a1487584bd912b2579489fa83fb07611a
c7araque <christianaraque@unicomfacauca.edu.co>
2024-03-11 01:39:47 -0500

    cambio de version en Angular

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  isLoggedIn = false;
+  user:any = null;
+  banderaResgistrar = false;
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+    public logout(){
+        Swal.fire({
+          title: '¿Está seguro?',
+          text: "¡La sesion se cerrara!",
+          icon: 'warning',
+          confirmButtonColor: '#3d9970',
+          showConfirmButton: true,
+          showCancelButton: true,
+          denyButtonText: `Don't save`,
+          confirmButtonText: 'Ok',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.router.navigateByUrl('/login');
+          }        
+        })
+    }
+
+    Registrar():void{
+      if(this.user==null||this.user== ''){
+        this.banderaResgistrar=false;
+      }
+      else{
+        if(this.user.authorities[0].authority=="ADMIN"){
+          this.banderaResgistrar=true;
+        }else{
+          this.banderaResgistrar=false; 
+        }
+      }  
+    }
+
+}
---
+++ src/app/pages/registrar/comprador/comprador.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-comprador',
+  templateUrl: './comprador.component.html',
+  styleUrls: ['./comprador.component.css']
+})
+export class CompradorComponent implements OnInit {
+
+  public user = {
+    username: '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    identificacion: ''
+  }
+
+
+  constructor(private snack: MatSnackBar,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+  formSubmit() {
+    // console.log(this.user);
+    if (this.user.username == '' || this.user.username == null) {
+      this.snack.open('El nombre de usuario es requerido !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    if (this.user.password == '' || this.user.password == null) {
+      this.snack.open('La contraseña es requerida !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    if (this.user.nombre == '' || this.user.nombre == null) {
+      this.snack.open('El nombre es requerido !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    if (this.user.apellido == '' || this.user.apellido == null) {
+      this.snack.open('El apellido es requerido !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    if (this.user.email == '' || this.user.email == null) {
+      this.snack.open('El email es requerido !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    if (this.user.telefono == '' || this.user.telefono == null) {
+      this.snack.open('El telefono es requerido !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    if (this.user.identificacion == '' || this.user.identificacion == null) {
+      this.snack.open('La identificacion es requerida !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+
+    //Conexion Backend
+
+  }
+
+  exit() {
+    if (this.user.username == null||this.user.username != null) {
+      Swal.fire({
+        title: '¿Está seguro?',
+        text: "¡Si no le diste en Registrar tu usuario no se creara!",
+        icon: 'warning',
+        confirmButtonColor: '#3d9970',
+        showConfirmButton: true,
+        showCancelButton: true,
+        denyButtonText: `Don't save`,
+        confirmButtonText: 'Ok',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "http://localhost:4200/login"
+        }
+      })
+    }
+  }
+}
