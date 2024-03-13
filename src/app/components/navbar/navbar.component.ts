0eb69de40a88142d12df83705cb075ea7cc3a626
Daniel Eduardo Rojas Cortés <daniel.rojas@sunsetswh.com>
2024-03-12 23:51:06 -0500

    Se realizan ajustes a versiones, librerias que no funcionaba, error de versiones, instalación de librerías que faltaban

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,53 +5,49 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  
   isLoggedIn = false;
-  user:any = null;
+  user: any = null;
   banderaResgistrar = false;
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public logout() {
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: '¡La sesion se cerrara!',
+      icon: 'warning',
+      confirmButtonColor: '#3d9970',
+      showConfirmButton: true,
+      showCancelButton: true,
+      denyButtonText: `Don't save`,
+      confirmButtonText: 'Ok',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown',
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp',
+      },
+    }).then((result: { isConfirmed: any }) => {
+      if (result.isConfirmed) {
+        this.router.navigateByUrl('/login');
+      }
+    });
   }
 
-    public logout(){
-        Swal.fire({
-          title: '¿Está seguro?',
-          text: "¡La sesion se cerrara!",
-          icon: 'warning',
-          confirmButtonColor: '#3d9970',
-          showConfirmButton: true,
-          showCancelButton: true,
-          denyButtonText: `Don't save`,
-          confirmButtonText: 'Ok',
-          showClass: {
-            popup: 'animate__animated animate__fadeInDown'
-          },
-          hideClass: {
-            popup: 'animate__animated animate__fadeOutUp'
-          }
-        }).then((result) => {
-          if (result.isConfirmed) {
-            this.router.navigateByUrl('/login');
-          }        
-        })
-    }
-
-    Registrar():void{
-      if(this.user==null||this.user== ''){
-        this.banderaResgistrar=false;
+  Registrar(): void {
+    if (this.user == null || this.user == '') {
+      this.banderaResgistrar = false;
+    } else {
+      if (this.user.authorities[0].authority == 'ADMIN') {
+        this.banderaResgistrar = true;
+      } else {
+        this.banderaResgistrar = false;
       }
-      else{
-        if(this.user.authorities[0].authority=="ADMIN"){
-          this.banderaResgistrar=true;
-        }else{
-          this.banderaResgistrar=false; 
-        }
-      }  
     }
-
+  }
 }
